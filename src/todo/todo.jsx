acbfcf967bd16ce7aca17583d212c3e9879e6e69
JaksonFives <jpv.lic16@uea.edu.br>
2019-11-22 19:08:27 -0400

    Método Handle Criado

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -4,11 +4,21 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+
+    constructor(props) {
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+    handleAdd() {
+        console.log(this)
+    }
+
     render() {
         return (
             <div>
                 <PageHeader nome='Tarefas' nomePequeno='Cadastro' />
-                <TodoForm />
+                <TodoForm handleAdd={this.handleAdd} />
                 <TodoList />
             </div>
         )
